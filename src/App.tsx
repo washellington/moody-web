c542918aa4b0deb2fb8a147ebf5244421d95c245
William Shellington <washellington@gmail.com>
2020-01-17 22:32:06 -0500

    created login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from "react";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import logo from "./assets/title/moody_title.svg";
-import "./App.css";
+import "./App.scss";
+import { stringify } from "querystring";
 
+interface InitialValueProp {
+  email: string;
+  password: string;
+}
 const App: React.FC = () => {
+  const initialValue: InitialValueProp = {
+    email: "",
+    password: ""
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required(),
+    password: Yup.string().required()
+  });
+
+  const formik = useFormik({
+    initialValues: initialValue,
+    validationSchema: validationSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <form id="signUpForm">
+        <input name="email" type="text" placeholder="Email" />
+        <input name="password" type="text" placeholder="Password" />
+        <button type="submit">Login</button>
+        <button type="button">Create Account</button>
+      </form>
     </div>
   );
 };
